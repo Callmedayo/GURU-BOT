243eaea7ce7655fd9944f4ea998d8d80ede6ecfc
Callmedayo <153470791+Callmedayo@users.noreply.github.com>
2023-12-17 02:01:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['2349063682481', 'DAYO TV', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
